a5c522496964e7d0c14f418874ede363eed3237f
Lucas Abraao <Labraao957@gmail.com>
2024-03-12 22:17:41 -0300

    User update

--- src/main/java/com/courseJava/courseJava/resouces/UserResource.java
+++ src/main/java/com/courseJava/courseJava/resouces/UserResource.java
@@ -30,16 +30,22 @@ public class UserResource {
     }
 
     @PostMapping
-    public ResponseEntity<User> insert(@RequestBody User obj){
+    public ResponseEntity<User> insert(@RequestBody User obj) {
         obj = services.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         services.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = services.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
 
