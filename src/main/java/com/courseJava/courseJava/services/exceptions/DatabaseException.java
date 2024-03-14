5fd495dc555a5aa35ced7955bbd4e71ed35846c1
Lucas Abraao <Labraao957@gmail.com>
2024-03-13 22:11:50 -0300

    Exception handling - delete

---
+++ src/main/java/com/courseJava/courseJava/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.courseJava.courseJava.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
